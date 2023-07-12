8037b7c50638ae93c59ec9f370c1d73bdade4c53
Hyunsubs <dbsgustjq0104@gmail.com>
2023-07-12 14:32:12 +0900

    플레이 화면 구상
    123

--- GameProject/yhPlay.cpp
+++ GameProject/yhPlay.cpp
@@ -3,7 +3,9 @@
 #include "yhObject.h"
 #include "yhSceneChanger.h"
 #include "yhPlayer.h"
-
+#include "yhTransform.h"
+#include "yhResources.h"
+#include "yhBackGround.h"
 
 yh::Play::Play()
 {
@@ -16,8 +18,19 @@ yh::Play::~Play()
 void yh::Play::Initialize()
 {
 	SceneChanger* changer = object::Instantiate<SceneChanger>(eLayerType::Background);
+	Texture* image = Resources::Load<Texture>(L"overworld1", L"..\\Resources\\image\\Maps\\overworld1.bmp");
+	BackGround* bg = object::Instantiate<BackGround>(eLayerType::Background);
+	SpriteRenderer* bgsr = bg->AddComponent<SpriteRenderer>();
+	bgsr->SetImage(image);
+	
+
+
+	image = Resources::Load<Texture>(L"link", L"..\\Resources\\image\\Link\\link1.png");
 	Player* player = object::Instantiate<Player>(eLayerType::Player);
-	player->AddComponent<SpriteRenderer>();
+	player->GetComponent<Transform>()->SetPosition(Vector2(256.0f,224.0f));
+	SpriteRenderer* player_sr = player->AddComponent<SpriteRenderer>();
+	player_sr->SetImage(image);
+	player_sr->SetScale(Vector2(1.5f, 1.5f));
 }
 
 void yh::Play::Update()
